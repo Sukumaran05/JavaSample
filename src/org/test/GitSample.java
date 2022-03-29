a3cf8a1c9f849ee9541e38f239af62f9cf771630
Sukumaran05 <Sukumaran.ece84@gmail.com>
2022-03-30 03:06:08 +0530

    new process completed

---
+++ src/org/test/GitSample.java
@@ -0,0 +1,31 @@
+package org.test;
+
+
+
+public class GitSample {
+	
+	private void greensOmr() {
+		System.out.println("Greens Tech-OMR");
+	}
+	private void greensAdayar() {
+		System.out.println("Greens Tech-Adayar");
+	}
+	private void greensTambaram() {
+		System.out.println("Greens Tech-Tambaram");
+	}
+	private void greensVelacherry() {
+		System.out.println("Greens Tech-Velacherry");
+	}
+	private void greensAnnaNagar() {
+		System.out.println("Greens Tech-AnnaNagar");
+	}
+	public static void main(String[] args) {
+		GitSample loc = new GitSample();
+		loc.greensOmr();
+		loc.greensAdayar();
+		loc.greensTambaram();
+		loc.greensVelacherry();
+		loc.greensAnnaNagar();	
+	}
+
+}
